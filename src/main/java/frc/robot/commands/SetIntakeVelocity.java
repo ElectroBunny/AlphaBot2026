5c863c90fc5f6622ba12c241292851f95bdd262c
nirrahamim <nir972782@gmail.com>
2026-02-19 22:38:41 +0200

    added commends to intake

---
+++ src/main/java/frc/robot/commands/SetIntakeVelocity.java
@@ -0,0 +1,41 @@
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Intake;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class SetIntakeVelocity extends Command {
+ private Intake intake;
+  private double velocity;
+  
+
+  public SetIntakeVelocity(double velocity) {
+    this.velocity=velocity;
+    this.intake=Intake.getInstance();
+    addRequirements(intake);
+  
+  }
+
+
+  @Override
+  public void initialize() {
+        this.intake.setVelocity(this.velocity);
+
+  }
+
+  
+  @Override
+  public void execute() {}
+  
+
+  @Override
+  public void end(boolean interrupted) {
+    this.intake.stop();
+  }
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
