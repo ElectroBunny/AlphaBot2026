6eba84da5cf5ed86335c4ce2f7b91352f914242e
Ori Roitzaid <oriroitzaid@gmail.com>
2026-02-24 19:37:51 +0200

    added FuelContainer subsystem

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -33,5 +33,11 @@ public final class Constants {
 	public static final int INTAKE_P = 0;
 	public static final int INTAKE_I = 0;
 	public static final int INTAKE_D = 0;
-	  public static final int INTAKE_MOTOR_CURRENT_LIMIT = 50;
+	public static final int INTAKE_MOTOR_CURRENT_LIMIT = 50;
+	public static final double 	FUEL_CONTAINER_P = 0;
+	public static final double 	FUEL_CONTAINER_I = 0;
+	public static final double 	FUEL_CONTAINER_D = 0;
+	public static final int FuelContainer_MOTOR_CURRENT_LIMIT = 5;
+	public static final int FuelContainer_MOTOR_ID = 0;
+
 }
